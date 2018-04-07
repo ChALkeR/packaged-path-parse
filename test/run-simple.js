e474a530f219e22ed31cbc9f2e6af4851be281ab
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-04-07 12:16:35 +0300

    test: make 'simpletest' runnable without any deps

--- test/run-simple.js
+++ test/run-simple.js
@@ -1,6 +1,21 @@
 'use strict';
 
-var tape = require('tape');
+var test;
+try {
+  test = require('tape');
+} catch (e) {
+  var assert = require('assert');
+  var keys = Object.keys(assert);
+  var t = {};
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    t[key] = assert[key];
+  }
+  t.end = function() {};
+  t.deepEqual = t.deepStrictEqual || t.deepEqual;
+  var test = function(name, run) { run(t); }
+}
+
 var es3impl = require('../index');
 var lib = require('./simple')
-lib.run(es3impl, tape, false);
+lib.run(es3impl, test, false);
